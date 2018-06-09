1d734fc0235d0c0288476d9486c5697e2e788dc8
Corefinder89 <soumyajit.basu62@gmail.com>
2018-06-09 11:22:02 +0530

    Make changes in the chrome_object.py

--- browserium/generic_functions/chrome_object.py
+++ browserium/generic_functions/chrome_object.py
@@ -8,7 +8,6 @@ class ChromeDriverObject(Utility, OS_type):
     # Pass the option 'headless' if it is needed to run chrome in headless
     # configuration
     def set_chromedriver_object(self, args=""):
-        os_name = self.os_name()
         try:
             driver = None
             driver_path = self.get_driver_path('/dependencies/dir_chromedriver/chromedriver')
@@ -28,28 +27,6 @@ class ChromeDriverObject(Utility, OS_type):
                     chrome_options=chromeOptions                        
                 )
                 self.log_message("INFO","Path for chrome binary is set")
-            if "linux" in os_name:
-                if not '--headless' in args:
-                    chromeOptions = webdriver.ChromeOptions()
-                    chromeOptions.add_argument(args)
-                    chromeOptions.add_experimental_option("useAutomationExtension", False)
-                    self.log_message("INFO","setting path of chromedriver")
-                    driver = webdriver.Chrome(
-                        executable_path=driver_path,
-                        chrome_options=chromeOptions                        
-                    )
-                    self.log_message("INFO","Path for chrome binary is set")
-                else:
-                    self.log_message("INFO","setting chrome into headless mode")
-                    chromeOptions = webdriver.ChromeOptions()
-                    chromeOptions.add_argument(args)
-                    chromeOptions.add_experimental_option("useAutomationExtension", False)
-                    self.log_message("INFO","setting path of chromedriver")
-                    driver = webdriver.Chrome(
-                        executable_path=driver_path, 
-                        chrome_options=chromeOptions                        
-                    )
-                    self.log_message("INFO","Path for chrome binary is set")
             return driver
         except WebDriverException as e:
             self.log_message("ERROR","There is an exception in the Web Driver configuration")
