5b72ea8d81615976f566d20641a913b94d7bd794
Corefinder89 <soumyajit.basu62@gmail.com>
2018-10-13 08:52:33 +0530

    Optimise code. Replace inheritance based coding with composition 	modified:   browserium/chromedriver.py 	modified:   browserium/geckodriver.py 	modified:   browserium/generic_functions/browser_controller.py 	modified:   browserium/generic_functions/chrome_object.py 	modified:   browserium/generic_functions/gecko_object.py 	modified:   browserium/generic_functions/logger.py 	modified:   browserium/generic_functions/opera_object.py 	modified:   browserium/generic_functions/safari_object.py 	modified:   browserium/logstashRunner/logstash_runner.py 	modified:   browserium/operadriver.py 	modified:   browserium/utility/os_type.py

--- browserium/geckodriver.py
+++ browserium/geckodriver.py
@@ -4,11 +4,15 @@ from selenium import webdriver
 import requests
 import os
 import shutil
-class Geckodriver(Utility, OS_type):
+class Geckodriver():
+
+    def __init__(self):
+        self.ut = Utility()
+        self.ot = OS_type()
     # Get the required chromedriver informations from the downloader_config.ini
     # Use the config_reader function from the Utility class to read the required configuration
     def geckodriver_object(self):
-        config_parser = self.config_reader()
+        config_parser = self.ut.config_reader()
         api_url = config_parser.get('GeckoDriver','latest_browser_driver')
         return api_url
 
@@ -16,61 +20,61 @@ class Geckodriver(Utility, OS_type):
     # geckodriver_objects function
     def parse_geckodriver_api(self):
         api_url = self.geckodriver_object()
-        browser_api_url = self.get_api_data(api_url)
+        browser_api_url = self.ut.get_api_data(api_url)
         return browser_api_url
 
     # Download the required geckodriver binary based on the operating system type
     def evaluate_on_environment(self, os_name, arch_type):
-        dir_path = self.get_driver_path('/dependencies/dir_geckodriver')
+        dir_path = self.ut.get_driver_path('/dependencies/dir_geckodriver')
         if os_name == 'macos' and arch_type == '64':
-            self.log_message("INFO","Environment: "+os_name)
-            self.log_message("INFO","Architecture Type: "+arch_type)
+            self.ut.log_message("INFO","Environment: "+os_name)
+            self.ut.log_message("INFO","Architecture Type: "+arch_type)
             url_builder_mac = self.parse_geckodriver_api()
-            self.log_message("INFO","Downloading the required binary for geckodriver")
-            self.driver_downloader(url_builder_mac['mac'], dir_path)
+            self.ut.log_message("INFO","Downloading the required binary for geckodriver")
+            self.ut.driver_downloader(url_builder_mac['mac'], dir_path)
             print "\n"
-            self.log_message("INFO", "Download completed")
-            self.untar_file('dir_geckodriver/')
-            self.log_message("INFO","Unarchiving contents completed")
+            self.ut.log_message("INFO", "Download completed")
+            self.ut.untar_file('dir_geckodriver/')
+            self.ut.log_message("INFO","Unarchiving contents completed")
         if os_name == 'linux' and arch_type == '64':
-            self.log_message("INFO","Environment: "+os_name)
-            self.log_message("INFO","Architecture Type: "+arch_type)
+            self.ut.log_message("INFO","Environment: "+os_name)
+            self.ut.log_message("INFO","Architecture Type: "+arch_type)
             url_builder_linux = self.parse_geckodriver_api()
-            self.log_message("INFO","Downloading the required binary for geckodriver")
-            self.driver_downloader(url_builder_linux['linux'], dir_path)
+            self.ut.log_message("INFO","Downloading the required binary for geckodriver")
+            self.ut.driver_downloader(url_builder_linux['linux'], dir_path)
             print "\n"
-            self.log_message("INFO", "Download completed")
-            self.untar_file('dir_geckodriver/')
-            self.log_message("INFO","Unarchiving contents completed")
+            self.ut.log_message("INFO", "Download completed")
+            self.ut.untar_file('dir_geckodriver/')
+            self.ut.log_message("INFO","Unarchiving contents completed")
         if os_name == 'linux' and arch_type == '32':
-            self.log_message("INFO","Environment: "+os_name)
-            self.log_message("INFO","Architecture Type: "+arch_type)
+            self.ut.log_message("INFO","Environment: "+os_name)
+            self.ut.log_message("INFO","Architecture Type: "+arch_type)
             url_builder_linux = self.parse_geckodriver_api()
-            self.log_message("INFO","Downloading the required binary for geckodriver")
-            self.driver_downloader(url_builder_linux['linux'], dir_path)
+            self.ut.log_message("INFO","Downloading the required binary for geckodriver")
+            self.ut.driver_downloader(url_builder_linux['linux'], dir_path)
             print "\n"
-            self.log_message("INFO", "Download completed")
-            self.untar_file('dir_geckodriver/')
-            self.log_message("INFO","Unarchiving contents completed")
+            self.ut.log_message("INFO", "Download completed")
+            self.ut.untar_file('dir_geckodriver/')
+            self.ut.log_message("INFO","Unarchiving contents completed")
 
     # Create a required directory separately for gecko and called the evaluate_on_environment
     # function to download the required binary
     def download_driver(self):
-        dir_path = self.get_driver_path('/dependencies/dir_geckodriver')
+        dir_path = self.ut.get_driver_path('/dependencies/dir_geckodriver')
         if os.path.exists(dir_path):
-            self.log_message("INFO","gecko driver is already present. To update gecko driver please run `browserium update --driver=geckodriver`")
+            self.ut.log_message("INFO","gecko driver is already present. To update gecko driver please run `browserium update --driver=geckodriver`")
         else:
             os.makedirs(dir_path)
-            os_name = self.os_name()
-            arch_type = str(self.os_architecture())
+            os_name = self.ot.os_name()
+            arch_type = str(self.ot.os_architecture())
             self.evaluate_on_environment(os_name, arch_type)
 
     # Update the required geckodriver based on the operating system type
     def update_driver(self):
-        self.log_message("INFO","Deleting directory contents")
-        self.check_directory_content("/dependencies/dir_geckodriver/geckodriver")
-        self.delete_dir_contents('dir_geckodriver/')
-        os_name = self.os_name()
-        arch_type = str(self.os_architecture())
+        self.ut.log_message("INFO","Deleting directory contents")
+        self.ut.check_directory_content("/dependencies/dir_geckodriver/geckodriver")
+        self.ut.delete_dir_contents('dir_geckodriver/')
+        os_name = self.ot.os_name()
+        arch_type = str(self.ot.os_architecture())
         self.evaluate_on_environment(os_name, arch_type)
-        self.log_message("INFO","geckodriver updated")
+        self.ut.log_message("INFO","geckodriver updated")
--- browserium/generic_functions/gecko_object.py
+++ browserium/generic_functions/gecko_object.py
@@ -3,29 +3,33 @@ from browserium.utility.utility import Utility
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import WebDriverException
 
-class GeckoDriverObject(Utility):
+class GeckoDriverObject():
+
+    def __init__(self):
+        self.ut = Utility()
+
     # Set geckodriver path
     # Pass the option 'headless' if it is needed to run gecko in headless
     # configuration
     def set_geckodriver_object(self, args=""):
         try:
             driver = None
-            driver_path = self.get_driver_path('/dependencies/dir_geckodriver/geckodriver')
+            driver_path = self.ut.get_driver_path('/dependencies/dir_geckodriver/geckodriver')
             if not '--headless' in args:
-                self.log_message("INFO","setting path of geckodriver")
+                self.ut.log_message("INFO","setting path of geckodriver")
                 driver = webdriver.Firefox(executable_path=driver_path)
-                self.log_message("INFO","executable path for geckodriver is set")
+                self.ut.log_message("INFO","executable path for geckodriver is set")
             else:
-                self.log_message("INFO","setting gecko into headless mode")
+                self.ut.log_message("INFO","setting gecko into headless mode")
                 firefoxOptions = Options()
                 firefoxOptions.add_argument(args)
-                self.log_message("INFO","setting path of geckodriver")
+                self.ut.log_message("INFO","setting path of geckodriver")
                 driver = webdriver.Firefox(
-                    executable_path=driver_path, 
-                    firefox_options=firefoxOptions                     
+                    executable_path=driver_path,
+                    firefox_options=firefoxOptions
                 )
-                self.log_message("INFO","executable path for geckodriver is set")
+                self.ut.log_message("INFO","executable path for geckodriver is set")
             return driver
         except WebDriverException as e:
-            self.log_message("ERROR","There is an exception in the Web Driver configuration")
+            self.ut.log_message("ERROR","There is an exception in the Web Driver configuration")
             print e
