433e580c53449cc9c30e1e82863f041cfaf67fdf
Corefinder89 <soumyajit.basu62@gmail.com>
2018-10-21 10:41:44 +0530

    Modify 1.gitignore file 2.browser controller class for added functionality of fluent wait 3. Modify chrome object and gecko object foradded chrome configs 	modified:   .gitignore 	modified:   TestFunction/UnitTest/test_gecko.py 	modified:   browserium/generic_functions/browser_controller.py 	modified:   browserium/generic_functions/chrome_object.py 	modified:   browserium/generic_functions/gecko_object.py

--- browserium/generic_functions/chrome_object.py
+++ browserium/generic_functions/chrome_object.py
@@ -10,26 +10,27 @@ class ChromeDriverObject():
     # Set chromedriver path
     # Pass the option 'headless' if it is needed to run chrome in headless
     # configuration
-    def set_chromedriver_object(self, args=""):
+    def set_chromedriver_object(self, args=[]):
         try:
             chromeOptions = webdriver.ChromeOptions()
             driver = None
             driver_path = self.ut.get_driver_path('/dependencies/dir_chromedriver/chromedriver')
             self.ut.set_executable_permission(driver_path)
             self.ut.log_message("INFO","setting executable permission to the chrome binary")
-            if not '--headless' in args:
-                self.ut.log_message("INFO","setting path of chromedriver")
-                driver = webdriver.Chrome(executable_path=driver_path)
-                self.ut.log_message("INFO","Path for chrome binary is set")
-            else:
-                self.ut.log_message("INFO","setting chrome into headless mode")
-                chromeOptions.add_argument(args)
-                self.ut.log_message("INFO","setting path of chromedriver")
-                driver = webdriver.Chrome(
-                    executable_path=driver_path,
-                    chrome_options=chromeOptions
-                )
-                self.ut.log_message("INFO","Path for chrome binary is set")
+            for val in args:
+                if not '--headless' in val:
+                    self.ut.log_message("INFO","setting path of chromedriver")
+                    driver = webdriver.Chrome(executable_path=driver_path)
+                    self.ut.log_message("INFO","Path for chrome binary is set")
+                else:
+                    self.ut.log_message("INFO","setting chrome into headless mode")
+                    chromeOptions.add_argument(val)
+                    self.ut.log_message("INFO","setting path of chromedriver")
+                    driver = webdriver.Chrome(
+                        executable_path=driver_path,
+                        chrome_options=chromeOptions
+                    )
+            self.ut.log_message("INFO","Path for chrome binary is set")
             return driver
         except WebDriverException as e:
             self.ut.log_message("ERROR","There is an exception in the Web Driver configuration")
--- browserium/generic_functions/gecko_object.py
+++ browserium/generic_functions/gecko_object.py
@@ -11,24 +11,25 @@ class GeckoDriverObject():
     # Set geckodriver path
     # Pass the option 'headless' if it is needed to run gecko in headless
     # configuration
-    def set_geckodriver_object(self, args=""):
+    def set_geckodriver_object(self, args=[]):
         try:
             driver = None
             driver_path = self.ut.get_driver_path('/dependencies/dir_geckodriver/geckodriver')
-            if not '--headless' in args:
-                self.ut.log_message("INFO","setting path of geckodriver")
-                driver = webdriver.Firefox(executable_path=driver_path)
-                self.ut.log_message("INFO","executable path for geckodriver is set")
-            else:
-                self.ut.log_message("INFO","setting gecko into headless mode")
-                firefoxOptions = Options()
-                firefoxOptions.add_argument(args)
-                self.ut.log_message("INFO","setting path of geckodriver")
-                driver = webdriver.Firefox(
-                    executable_path=driver_path,
-                    firefox_options=firefoxOptions
-                )
-                self.ut.log_message("INFO","executable path for geckodriver is set")
+            for val in args:
+                if not '--headless' in val:
+                    self.ut.log_message("INFO","setting path of geckodriver")
+                    driver = webdriver.Firefox(executable_path=driver_path)
+                    self.ut.log_message("INFO","executable path for geckodriver is set")
+                else:
+                    self.ut.log_message("INFO","setting gecko into headless mode")
+                    firefoxOptions = Options()
+                    firefoxOptions.add_argument(val)
+                    self.ut.log_message("INFO","setting path of geckodriver")
+                    driver = webdriver.Firefox(
+                        executable_path=driver_path,
+                        firefox_options=firefoxOptions
+                    )
+            self.ut.log_message("INFO","executable path for geckodriver is set")
             return driver
         except WebDriverException as e:
             self.ut.log_message("ERROR","There is an exception in the Web Driver configuration")
