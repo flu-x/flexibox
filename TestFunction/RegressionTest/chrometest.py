905a29430d2a2fec17af7d775131ccc74e7c7bf3
Corefinder <synup@synups-MacBook-Pro-2.local>
2019-11-01 21:02:47 +0530

    Make modifications in chrometest and firefoxtest 	modified:   TestFunction/RegressionTest/chrometest.py 	modified:   TestFunction/RegressionTest/firefoxtest.py

--- TestFunction/RegressionTest/chrometest.py
+++ TestFunction/RegressionTest/chrometest.py
@@ -9,6 +9,7 @@ class Test_1():
         controller.implicit_wait_time(driver, 4)
         current_url = controller.get_current_url(driver)
         print(current_url)
+        controller.tear_browser(driver)
 
 t = Test_1()
 t.test_chromedriver_type()
