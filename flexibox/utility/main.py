445215251d876d52f2136033841c6a89c756915d
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-02-18 16:23:25 +0530

    modify delete command 	modified:   flexibox/utility/main.py

--- flexibox/utility/main.py
+++ flexibox/utility/main.py
@@ -12,11 +12,11 @@ subparser = parser.add_subparsers(dest='command', help="download")
 
 download = subparser.add_parser("download", help="download driver binary")
 update = subparser.add_parser("update", help="update driver binary")
-delete = subparser.add_parser("delete", help="delete specific driver binary")
+deleteall = subparser.add_parser("delete", help="delete specific driver binary")
 
 download.add_argument("--driver", help="download the respective driver")
 update.add_argument("--driver", help="update the respective driver")
-delete.add_argument("--driver", help="delete all drivers")
+# deleteall.add_argument("--driver", help="delete all drivers")
 
 
 def main():
@@ -27,7 +27,7 @@ def main():
         driver_object.download_driver()
     if args.command == "update":
         driver_object.update_driver()
-    if args.command == "delete":
+    if args.command == "deleteall":
         driver_object.delete_driver_history()
 
 
