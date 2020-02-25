41375a71979fe7059593096c9dd73ee7ac038a1f
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-02-25 15:51:32 +0530

    Remove unnecessary codebase from logger class 	modified:   flexibox/core/logger.py

--- flexibox/core/logger.py
+++ flexibox/core/logger.py
@@ -15,35 +15,20 @@ sentry_sdk.init(dsn="https://bc26766d1b0c464f80a60d18394c1585@sentry.io/2844244"
 class Logger(object):
     @classmethod
     def log_info(self, informationMessage):
-        # logging.basicConfig(
-        #     format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO
-        # )
         logging.info(informationMessage)
 
     @classmethod
     def log_error(self, errorMessage):
-        # logging.basicConfig(
-        #     format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.ERROR
-        # )
         logging.error(errorMessage)
 
     @classmethod
     def log_debug(self, debugMessage):
-        # logging.basicConfig(
-        #     format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG
-        # )
         logging.debug(debugMessage)
 
     @classmethod
     def log_warning(self, warningMessage):
-        # logging.basicConfig(
-        #     format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.WARNING
-        # )
         logging.warning(warningMessage)
 
     @classmethod
     def log_critical(self, criticalMessage):
-        # logging.basicConfig(
-        #     format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.CRITICAL
-        # )
         logging.critical(criticalMessage)
