c8dcf5ac1d1648fb6ed2f88af85087361642342c
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-02-26 10:37:31 +0530

    Make changes to logger.py 	modified:   ../flexibox/core/logger.py

--- flexibox/core/logger.py
+++ flexibox/core/logger.py
@@ -4,31 +4,37 @@
 # Purpose: The purpose of the class Logger is to have all the generic functionality for logging
 # Can be used to generate logs for all the required functions carried out in the project
 import logging
+from time import sleep
 
 import sentry_sdk
 from sentry_sdk.integrations.logging import LoggingIntegration
 
-sentry_logging = LoggingIntegration(level=logging.INFO, event_level=logging.ERROR)
-sentry_sdk.init(dsn="https://5b823e3b2d184f829b8ec52bfcd40a84@sentry.io/2944174", integrations=[sentry_logging])
-
 
 class Logger(object):
+    sentry_logging = LoggingIntegration(level=logging.INFO, event_level=logging.ERROR)
+    sentry_sdk.init(dsn="https://5b823e3b2d184f829b8ec52bfcd40a84@sentry.io/2944174", integrations=[sentry_logging])
+
     @classmethod
     def log_info(self, informationMessage):
         logging.info(informationMessage)
+        sleep(2)
 
     @classmethod
     def log_error(self, errorMessage):
         logging.error(errorMessage)
+        sleep(2)
 
     @classmethod
     def log_debug(self, debugMessage):
         logging.debug(debugMessage)
+        sleep(2)
 
     @classmethod
     def log_warning(self, warningMessage):
         logging.warning(warningMessage)
+        sleep(2)
 
     @classmethod
     def log_critical(self, criticalMessage):
         logging.critical(criticalMessage)
+        sleep(2)
