003e873f07f1735f53b250f144cf8183d7c977dc
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-06-17 11:37:10 +0530

    Remove sentry integration 	modified:   flexibox/core/logger.py

--- flexibox/core/logger.py
+++ flexibox/core/logger.py
@@ -6,35 +6,24 @@
 import logging
 from time import sleep
 
-import sentry_sdk
-from sentry_sdk.integrations.logging import LoggingIntegration
-
 
 class Logger(object):
-    sentry_logging = LoggingIntegration(level=logging.INFO, event_level=logging.ERROR)
-    sentry_sdk.init(dsn="https://5b823e3b2d184f829b8ec52bfcd40a84@sentry.io/2944174", integrations=[sentry_logging])
-
-    @classmethod
     def log_info(self, informationMessage):
         logging.info(informationMessage)
         sleep(2)
 
-    @classmethod
     def log_error(self, errorMessage):
         logging.error(errorMessage)
         sleep(2)
 
-    @classmethod
     def log_debug(self, debugMessage):
         logging.debug(debugMessage)
         sleep(2)
 
-    @classmethod
     def log_warning(self, warningMessage):
         logging.warning(warningMessage)
         sleep(2)
 
-    @classmethod
     def log_critical(self, criticalMessage):
         logging.critical(criticalMessage)
         sleep(2)
