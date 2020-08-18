625869c29be4f530f37a504ee3fc8eafa662e3ac
Soumyajit Basu (C0r3f!nd3r) <soumyajit.basu62@gmail.com>
2020-08-18 10:59:14 +0530

    Make changes to the logger and page_object cl 	modified:   flexibox/core/logger.py 	modified:   flexibox/core/page_object.py

--- flexibox/core/logger.py
+++ flexibox/core/logger.py
@@ -2,7 +2,7 @@
 # Language: Python
 # Copyrights: SoumyajitBasu
 # Purpose: The purpose of the class Logger is to have all the generic functionality for logging
-# Can be used to generate logs for all the required functions carried out in the project
+#          Can be used to generate logs for all the required functions carried out in the project
 import logging
 from time import sleep
 
@@ -10,20 +10,16 @@ from time import sleep
 class Logger(object):
     def log_info(self, informationMessage):
         logging.info(informationMessage)
-        sleep(2)
+        sleep(1)
 
     def log_error(self, errorMessage):
         logging.error(errorMessage)
-        sleep(2)
 
     def log_debug(self, debugMessage):
         logging.debug(debugMessage)
-        sleep(2)
 
     def log_warning(self, warningMessage):
         logging.warning(warningMessage)
-        sleep(2)
 
     def log_critical(self, criticalMessage):
         logging.critical(criticalMessage)
-        sleep(2)
--- flexibox/core/page_object.py
+++ flexibox/core/page_object.py
@@ -36,54 +36,54 @@ class Pageobject(object):
     def get_element_by_xpath(self, driver, xpath):
         try:
             driver.find_element_by_xpath(driver, xpath)
-            self.log.log_error("Element with the xpath is traced on the web page")
+            self.log.log_error("Element with the XPATH is traced on the web page")
         except NoSuchElementException:
-            self.log.log_error("Element with the xpath is not present on the web page")
+            self.log.log_error("Element with the XPATH is not present on the web page")
 
     # Get element by link text
     def get_element_by_linktext(self, driver, link_text):
         try:
             driver.find_element_by_link_text(link_text)
-            self.log.log_info("Link text is found on the web page")
+            self.log.log_info("LINK TEXT is found on the web page")
         except NoSuchElementException:
-            self.log.log_error("Link text is not present on the web page")
+            self.log.log_error("LINK TEXT is not present on the web page")
 
     # Get element by partial link text
     def get_element_by_partial_linktext(self, driver, partial_link_text):
         try:
             driver.find_element_by_link_text(partial_link_text)
-            self.log.log_info("Partial link text is found on the web page")
+            self.log.log_info("PARTIAL LINK TEXT is found on the web page")
         except NoSuchElementException:
-            self.log.log_error("Partial link text is not present on the web page")
+            self.log.log_error("PARTIAL LINK TEXT is not present on the web page")
 
     # Get element by tag name
     def get_element_by_tagname(self, driver, tag_name):
         try:
             driver.find_element_by_tag_name(tag_name)
-            self.log.log_info("Tag name is present on the web page")
+            self.log.log_info("TAG NAME is present on the web page")
         except NoSuchElementException:
-            self.log.log_error("Tag name is not present on the web page")
+            self.log.log_error("TAG NAME is not present on the web page")
 
     # Get element by class name
     def get_element_by_class_name(self, driver, class_name):
         try:
             driver.find_element_by_class_name(class_name)
-            self.log.log_info("Class name is not found on the web page")
+            self.log.log_info("CLASS NAME is not found on the web page")
         except NoSuchElementException:
-            self.log.log_error("Class name is not present on the web page")
+            self.log.log_error("CLASS NAME is not present on the web page")
 
     # Get element by css selector
     def get_element_by_css_selector(self, driver, selector):
         try:
             driver.find_element_by_css_selector(selector)
         except NoSuchElementException:
-            self.log.log_error("Selector is not present in the web page")
+            self.log.log_error("CSS SELECTOR is not present in the web page")
 
     # Return elements by name as a list
     def get_elements_by_name(self, driver, name):
         try:
             driver.find_elements_by_name(name)
-            self.log.log_info("elements with attribute name is traced on the web page")
+            self.log.log_info("Elements with attribute NAME is traced on the web page")
         except NoSuchelementsException:
             self.log.log_error("Attribute NAME is not present on the web page")
 
@@ -91,45 +91,45 @@ class Pageobject(object):
     def get_elements_by_xpath(self, driver, xpath):
         try:
             driver.find_elements_by_xpath(driver, xpath)
-            self.log.log_error("elements with the xpath is traced on the web page")
+            self.log.log_error("Elements with the XPATH is traced on the web page")
         except NoSuchelementsException:
-            self.log.log_error("elements with the xpath is not present on the web page")
+            self.log.log_error("Elements with the XPATH is not present on the web page")
 
     # Return elements by link text as a list
     def get_elements_by_linktext(self, driver, link_text):
         try:
             driver.find_elements_by_link_text(link_text)
-            self.log.log_info("Link text is found on the web page")
+            self.log.log_info("LINK TEXT is found on the web page")
         except NoSuchelementsException:
-            self.log.log_error("Link text is not present on the web page")
+            self.log.log_error("LINK TEXT is not present on the web page")
 
     # Return elements by partial link text as a list
     def get_elements_by_partial_linktext(self, driver, partial_link_text):
         try:
             driver.find_elements_by_link_text(partial_link_text)
-            self.log.log_info("Partial link text is found on the web page")
+            self.log.log_info("PARTIAL LINK TEXT is found on the web page")
         except NoSuchelementsException:
-            self.log.log_error("Partial link text is not present on the web page")
+            self.log.log_error("PARTIAL LINK TEXT is not present on the web page")
 
     # Return elements by tag name as a list
     def get_elements_by_tagname(self, driver, tag_name):
         try:
             driver.find_elements_by_tag_name(tag_name)
-            self.log.log_info("Tag name is present on the web page")
+            self.log.log_info("TAG NAME is present on the web page")
         except NoSuchelementsException:
-            self.log.log_error("Tag name is not present on the web page")
+            self.log.log_error("TAG NAME is not present on the web page")
 
     # Return elements by class name as a list
     def get_elements_by_class_name(self, driver, class_name):
         try:
             driver.find_elements_by_class_name(class_name)
-            self.log.log_info("Class name is not found on the web page")
+            self.log.log_info("CLASS NAME is not found on the web page")
         except NoSuchelementsException:
-            self.log.log_error("Class name is not present on the web page")
+            self.log.log_error("CLASS NAME is not present on the web page")
 
     # Return elements by css selector as a list
     def get_elements_by_css_selector(self, driver, selector):
         try:
             driver.find_elements_by_css_selector(selector)
         except NoSuchelementsException:
-            self.log.log_error("Selector is not present in the web page")
+            self.log.log_error("CSS SELECTOR is not present in the web page")
